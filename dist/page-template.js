c441647471080bfc12bcd3d15fd921b331a54ed6
Ethan Cahill <andersmith29@gmail.comhtml lang=en  head    meta charset=UTF-8 /  /head  body    p      I LOVE PANDASgit config --global user.email andersmith29@gmail.com>
2022-05-01 12:27:30 -0400

    added generateIntern/Engineer functions

--- dist/page-template.js
+++ dist/page-template.js
@@ -22,4 +22,49 @@ const generateManager = manager => {
     `
 }
 
+const generateIntern = intern => {
+    return `
+    <section class="container">
+        <div class="row">
+            <div class="col">
+                <div id="internTitle">
+                    <h2>
+                        ${intern.role}
+                    </h2>
+                    <h3>
+                        ${intern.name}
+                    </h3>
+                </div>
+                <div id="internInfo">
+                    <p> ID: ${intern.id} </p>
+                    </b>
+                    <a href = "mailto: ${intern.email}"> ${manager.email}</a>
+                    </b>
+                    <p> School: ${intern.school}</p>
+                </div>
+    `
+}
+
+const generateEngineer = engineer => {
+    return `
+    <section class="container">
+    <div class="row">
+        <div class="col">
+            <div id="engineerTitle">
+                <h2>
+                    ${engineer.role}
+                </h2>
+                <h3>
+                    ${engineer.name}
+                </h3>
+            </div>
+            <div id="engineerInfo">
+                <p> ID: ${engineer.id} </p>
+                </b>
+                <a href = "mailto: ${engineer.email}"> ${engineer.email}</a>
+                </b>
+                <p> School: ${engineer.school}</p>
+            </div>`
+}
+
 module.exports = generateManager
